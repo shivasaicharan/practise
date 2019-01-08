e751d208902a3e9e0f8d761f0aa5230db051c0f8
shivasaicharan <46471584+shivasaicharan@users.noreply.github.com>
2019-01-08 10:06:39 +0530

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,27 @@
+function MyFunction(){
+    var x=document.getElementsByClassName("hide");
+    for(var i = 0; i < x.length; i++){
+      x[i].style.display="none";
+    }
+  }
+  function display(){
+    document.getElementById("myworld").innerHTML="Have a nice day!";
+  }
+  function bulb(s){
+    var pic;
+    if(s==0)
+      pic="pic_bulboff.gif";
+    else
+      pic="pic_bulbon.gif";
+    document.getElementById("myimage").src=pic;
+  }
+  function date(){
+   var x=new Date("2018-2-30");
+   document.getElementById("date").innerHTML=Number(x);
+  }
+  function sort(){
+    var p=[10,20,40,1,4];
+    document.getElementById("sort").innerHTML=p;
+    p.sort(function(a,b) {return(a-b)});
+    document.getElementById("sort").innerHTML=p;
+  }
