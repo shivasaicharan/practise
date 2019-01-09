0105bb8af7ccea8066fd9fc4afbab75a734524b8
shivasaicharan <46471584+shivasaicharan@users.noreply.github.com>
2019-01-09 19:25:57 +0530

    today update

--- 09-01-2019/celsius,fahrenheit converter/celsius-fahrenheit.js
+++ 09-01-2019/celsius,fahrenheit converter/celsius-fahrenheit.js
@@ -1,10 +1,11 @@
 function ctof(){
     var c=document.getElementById("temp").value;
     var f=(c/5)*9+32;
-    document.getElementById("t").innerHTML=f;
+    document.getElementById("t").innerHTML=c+'\xB0C is ' + f + '\xB0F.';
+    ;
 }
 function ftoc(){
     var f=document.getElementById("temp").value;
     var c=5*(f-32)/9;
-    document.getElementById("t").innerHTML=c;
+    document.getElementById("t").innerHTML=f+'\xB0F is ' + c + '\xB0C.';
 }
