7018af25322e75a2ef8cbf922dcdd8919e0f53e8
shivasaicharan <46471584+shivasaicharan@users.noreply.github.com>
2019-01-22 22:04:03 +0530

    todays update

--- 22-01-2019/Object details/details.js
+++ 22-01-2019/Object details/details.js
@@ -9,12 +9,11 @@ function add(){
     document.getElementById("str1").value="";
     document.getElementById("str2").value="";
     document.getElementById("num").value="";
-    console.log(pen);
 }
 function display(){
     var c="";
-    for(i=0;i<a.length-1;i++){
-        c=c+JSON.stringify(a,null,a.length);
+    for(i=0;i<a.length;i++){
+        c=c+"Pen "+(i+1)+" details<br><br>"+"Color: "+a[i].color+"<br>Brand: "+a[i].brand+"<br>Price: "+a[i].price+"<br><br>";
     }
     document.getElementById("result").innerHTML=c;
 }
