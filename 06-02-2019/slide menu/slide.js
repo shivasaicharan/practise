d50c4601dcdd508761c202c63bbce7f86367a343
shivasaicharan <46471584+shivasaicharan@users.noreply.github.com>
2019-02-07 09:42:33 +0530

    todays update

--- 06-02-2019/slide menu/slide.js
+++ 06-02-2019/slide menu/slide.js
@@ -1,6 +1,6 @@
 function openmenu() {
-    document.getElementById("menu").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("menu").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
   }
   
   function closemenu() {
